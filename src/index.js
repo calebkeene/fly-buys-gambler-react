77c8d0260d96fefc08b450ca4a09ea28c9743e59
calebkeene <caleb@hashbang.co.nz>
2018-09-01 13:42:01 +1200

    remove some cruft, add react-router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './js/components/App';
 import registerServiceWorker from './js/services/registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
